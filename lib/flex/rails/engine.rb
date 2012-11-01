660682d2ea6ecf5d2f9219b38f49d9fc770e27a2
Domizio Demichelis <dd.nexus@gmail.com>
2012-11-01 21:26:18 +0100

    added config.flex in engine

--- lib/flex/rails/engine.rb
+++ lib/flex/rails/engine.rb
@@ -3,10 +3,10 @@ module Flex
     class Engine < ::Rails::Engine
 
       ActiveSupport.on_load(:before_configuration) do
-        Flex::Configuration.configure do |c|
-          c.variables[:index] = [self.class.name.split('::').first.underscore, ::Rails.env].join('_')
-          c.config_file       = ::Rails.root.join('config', 'flex.yml').to_s
-          c.flex_dir          = ::Rails.root.join('app', 'flex').to_s
+        config.flex = Flex::Configuration
+        config.flex.variables[:index] = [self.class.name.split('::').first.underscore, ::Rails.env].join('_')
+        config.flex.config_file       = ::Rails.root.join('config', 'flex.yml').to_s
+        config.flex.flex_dir          = ::Rails.root.join('app', 'flex').to_s
         end
       end
 
