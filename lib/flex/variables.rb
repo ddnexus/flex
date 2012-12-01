0bb56b5e0d3762d327029621e3683f93c6e7e81b
Domizio Demichelis <dd.nexus@gmail.com>
2012-12-01 22:51:39 +0100

    variables refactoring: auto symbolized keys and deep_merg on new

--- lib/flex/variables.rb
+++ lib/flex/variables.rb
@@ -3,9 +3,40 @@ module Flex
 
     include Structure::Mergeable
 
-    def initialize(hash=nil)
-      hash ||= {} # accepts an explicit nil
-      replace hash
+    def initialize(*hashes)
+      deep_merge! *hashes
+    end
+
+    def merge(hash)
+      super symbolize(hash)
+    end
+
+    def merge!(hash)
+      super symbolize(hash)
+    end
+
+    def store(key, val)
+      super key.to_sym, symbolize(val)
+    end
+    alias_method :[]=, :store
+
+  private
+
+    def symbolize(struct)
+      case struct
+      when Flex::Variables
+        struct
+      when Hash
+        h = Variables.new
+        struct.each do |k,v|
+          h[k.to_sym] = symbolize(v)
+        end
+        h
+      when Array
+        struct.map{|i| symbolize(i)}
+      else
+        struct
+      end
     end
 
   end
