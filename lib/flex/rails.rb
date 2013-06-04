3dc78ac72ce8e92ed74173bfdb36d1dbb3a704ac
Domizio Demichelis <dd.nexus@gmail.com>
2013-06-04 13:58:24 +0200

    better feedback for deprecated require flex/rails

--- lib/flex/rails.rb
+++ lib/flex/rails.rb
@@ -1,1 +1,1 @@
-raise NotImplementedError, 'The "flex/rails" file has been replaced by the "flex-rails" gem. Please, use flex-rails in place of flex in Rails applications.'
+raise NotImplementedError, %(The "flex/rails" file has been replaced by the "flex-rails" gem. Please, use "flex-rails" in place of "flex" in Rails applications (change "gem 'flex', require => 'flex/rails'" to "gem 'flex-rails'" in your Gemfile))
