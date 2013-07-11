c7ae2ca34354ecbfe113e8e8d7de8e3984131569
Domizio Demichelis <dd.nexus@gmail.com>
2013-07-11 22:08:26 +0200

    moved the nil data support for post and put in patron

--- lib/flex/http_clients/patron.rb
+++ lib/flex/http_clients/patron.rb
@@ -3,6 +3,10 @@ module Flex
     class Patron < Base
 
       def request(method, path, data=nil)
+        # patron would raise an error for :post and :put requests with no data
+        # and elasticsearch ignores the data when it expects no data,
+        # so we silence patron by adding some dummy data
+        data = {} if method == 'POST' || method == 'PUT' && data.nil?
         opts = options.merge(:data => data)
         session.request method.to_s.downcase.to_sym, path, {}, opts
       rescue ::Patron::TimeoutError
