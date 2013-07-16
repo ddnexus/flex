31ecba2f681d548ab152cb20a66ff64554d7752b
Domizio Demichelis <dd.nexus@gmail.com>
2013-07-16 11:29:12 +0200

    fix for missing parens in patron http client

--- lib/flex/http_clients/patron.rb
+++ lib/flex/http_clients/patron.rb
@@ -6,7 +6,7 @@ module Flex
         # patron would raise an error for :post and :put requests with no data
         # and elasticsearch ignores the data when it expects no data,
         # so we silence patron by adding some dummy data
-        data = {} if method == 'POST' || method == 'PUT' && data.nil?
+        data = {} if (method == 'POST' || method == 'PUT') && data.nil?
         opts = options.merge(:data => data)
         session.request method.to_s.downcase.to_sym, path, {}, opts
       rescue ::Patron::TimeoutError
