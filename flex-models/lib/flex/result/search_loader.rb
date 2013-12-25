158dcf3c568008d0151ee6cb6df900f295f73cf8
Andrey Deryabin <deriabin@gmail.com>
2013-12-25 20:18:04 +0400

    move loading collection to separate module

--- flex-models/lib/flex/result/search_loader.rb
+++ flex-models/lib/flex/result/search_loader.rb
@@ -13,28 +13,35 @@ module Flex
       end
 
       def loaded_collection
-        @loaded_collection ||= begin
-                                 records  = []
-                                 # returns a structure like {Comment=>[{"_id"=>"123", ...}, {...}], BlogPost=>[...]}
-                                 h = Utils.group_array_by(collection) do |d|
-                                   d.model_class
-                                 end
-                                 h.each do |klass, docs|
-                                   records |= klass.find(docs.map(&:_id))
-                                 end
-                                 class_ids = collection.map { |d| [d.model_class.to_s,  d._id] }
-                                 # Reorder records to preserve order from search results
-                                 records = class_ids.map do |class_str, id|
-                                   records.detect do |record|
-                                     record.class.to_s == class_str && record.id.to_s == id.to_s
-                                   end
-                                 end
-                                 records.extend Struct::Paginable
-                                 records.setup(collection.total_entries, variables)
-                                 records
-                               end
+        @loaded_collection ||= load_collection
+
       end
 
+      private
+
+      def load_collection
+        records  = []
+        # returns a structure like {Comment=>[{"_id"=>"123", ...}, {...}], BlogPost=>[...]}
+        h = Utils.group_array_by(collection) do |d|
+          d.model_class
+        end
+
+        h.each do |klass, docs|
+          records |= klass.find(docs.map(&:_id))
+        end
+
+        class_ids = collection.map { |d| [d.model_class.to_s,  d._id] }
+        # Reorder records to preserve order from search results
+        records = class_ids.map do |class_str, id|
+          records.detect do |record|
+            record.class.to_s == class_str && record.id.to_s == id.to_s
+          end
+        end
+
+        records.extend Struct::Paginable
+        records.setup(collection.total_entries, variables)
+        records
+      end
     end
   end
 end
